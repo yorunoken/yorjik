139a8d745a08cc0135d8bd20f6fdfd642e3bcd53
Yoru <me@yorunoken.com>
2025-09-12 13:11:04 +0300

    commentcommentcommentcommentcommentcommentcomment

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
-    // Load the environment variables
+    // load env variables
     dotenv().ok();
 
-    // Initialize database
+    // initialize database
     let database = Arc::new(
         database::Database::new("sqlite:data.db")
             .await
@@ -27,7 +27,7 @@ async fn main() {
     let commands = commands::commands_vecs();
     let registered = commands::register_vecs();
 
-    // Build the Discord client, and pass in our event handler
+    // build the Discord client, and pass in our event handler
     let mut client = Client::builder(discord_token, intents)
         .event_handler(event_handler::Handler {
             commands,
@@ -37,7 +37,7 @@ async fn main() {
         .await
         .expect("Error creating client.");
 
-    // Run the Discord client (runs the ready function)
+    // run the client
     if let Err(reason) = client.start().await {
         println!("Error starting client: {:?}", reason);
     }
